b1be878afe7df42f651b5d166d89b32c279755f5
joaoricteixeira <76978819+joaoricteixeira@users.noreply.github.com>
2025-10-20 19:08:11 +0100

    File creation

---
+++ get_next_line.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joateixe <joateixe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/20 16:05:40 by joateixe          #+#    #+#             */
+/*   Updated: 2025/10/20 16:07:05 by joateixe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*get_next_line(int fd)
+{
+
+
+}
+
+#include <unistd.h>
+#include <sys/types.h>
+int	main(int argc, char **argv)
+{
+	int	fd;
+	if (argc == 2)
+	{}		fd = open(argv[1], O_RDONLY);
+
+		fd = open(argv[1], O_RDONLY);
+	
+
+}
